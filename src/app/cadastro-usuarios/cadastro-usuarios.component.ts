615bc16120e700bc19d5110741234dfc2d5e3e05
Ezequiel Brilhante <ezequielrb7@gmail.com>
2022-03-13 00:40:38 -0300

    Criando o add usuarios com ngrx

--- src/app/cadastro-usuarios/cadastro-usuarios.component.ts
+++ src/app/cadastro-usuarios/cadastro-usuarios.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { UsuarioModel } from '../Models/UsuarioModel';
+import { AppState } from '../Store/app-state';
 import { UsuariosService } from './../Repository/UsuariosService';
+import * as fromUsuariosActions from '../Store/usuarios/usuarios.actions';
 
 @Component({
   selector: 'app-cadastro-usuarios',
@@ -10,15 +13,21 @@ import { UsuariosService } from './../Repository/UsuariosService';
 export class CadastroUsuariosComponent implements OnInit {
   model: UsuarioModel = { id: 0, nome: '', idade: 0, perfil: '' };
 
-  constructor(private usuarioService: UsuariosService) {}
+  constructor(
+    // private usuarioService: UsuariosService,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit(): void {}
 
   addUsuario() {
     if (this.model.id == 0) {
-      this.usuarioService.addUsuario(this.model).subscribe();
+      // this.usuarioService.addUsuario(this.model).subscribe();
+      this.store.dispatch(
+        fromUsuariosActions.CreateUsuario({ payload: this.model })
+      );
     } else {
-      this.usuarioService.updateUsuario(this.model);
+      // this.usuarioService.updateUsuario(this.model);
     }
   }
 }
